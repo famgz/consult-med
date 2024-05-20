f62323e4c39e40c1d0c3f49fa759f4ebb8f171e0
famgz <famgz@proton.me>
2024-05-20 18:51:45 -0300

    feat: ensure create appointment button will not appear to admin user on header

--- src/app/commons/components/header/header.component.ts
+++ src/app/commons/components/header/header.component.ts
@@ -3,12 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 import { AuthService } from './../../../modules/auth/services/auth.service';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterLink, AsyncPipe],
+  imports: [MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
