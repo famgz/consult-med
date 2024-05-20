cfa9414d0ed3b25a335950eb9b7eb8fe16f09cbe
famgz <famgz@proton.me>
2024-05-20 17:51:15 -0300

    refactor: hero image as component

---
+++ src/app/commons/components/hero-image/hero-image.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroImageComponent } from './hero-image.component';
+
+describe('HeroImageComponent', () => {
+  let component: HeroImageComponent;
+  let fixture: ComponentFixture<HeroImageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeroImageComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(HeroImageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/commons/components/hero-image/hero-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-image.component.html',
+  styleUrl: './hero-image.component.scss'
+})
+export class HeroImageComponent {
+
+}
